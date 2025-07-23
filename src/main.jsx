87828a0925c34a59a07316d31e5643290a40e6c9
hayyantahirr <hayyantahir7@gmail.com>
2025-07-23 20:29:29 +0500

    screens and components add + routing set up

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Home from "./screens/Home";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "*", element: <Home /> },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   
-    
+    <RouterProvider router={router} />
   
-)
+);
