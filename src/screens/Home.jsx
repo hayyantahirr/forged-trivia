273f4f5ac3b7fe606743a9986a277540c69325ec
hayyantahirr <hayyantahir7@gmail.com>
2025-07-23 23:26:21 +0500

    Heading Done

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <></>
-  )
-}
+    <>
+      <div className=" mx-auto w-[85%] my-10">
+        <h1 className="text-3xl font-sans font-black mb-5 text-start ">Test Your Wit ,<span className="text-[#da7e36]"> Forge Your Legacy</span> </h1>
+        <p className="text-xl text-[#da7e36] opacity-70">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
+          voluptates facere accusantium dolore eligendi aliquid vero minima quam
+          odit at velit iste error totam aliquam optio dicta, perferendis
+          voluptatibus molestias? Dolorem quod cupiditate adipisci consequatur,
+          placeat ipsum. Inventore ipsam nam unde? Consequuntur ex quam fugit,
+          labore, sunt nihil eius quidem aliquam ipsa nesciunt voluptatum
+          possimus eveniet. Labore quidem libero magni! Laboriosam animi dolorum
+          voluptatem delectus quaerat, nihil temporibus quidem sapiente
+          distinctio eveniet? Eius deserunt alias sint dolorum beatae
+          accusantium iure quia, fuga earum nemo ut officia, atque consequatur,
+          molestiae non. Iure dolor ab, et exercitationem quasi debitis officia,
+          vel velit modi fugiat aliquid consectetur ut libero doloremque
+          provident officiis perferendis totam sunt? Eaque neque voluptatem est
+          doloremque eum odio ab?
+        </p>
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
