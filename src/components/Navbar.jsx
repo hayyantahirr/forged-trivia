33f2d367c94b97412faf088a612edbb2bd852ba6
hayyantahirr <hayyantahir7@gmail.com>
2025-07-25 02:03:51 +0500

    Quiz App Completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,13 @@ import React from "react";
 const Navbar = () => {
   return (
     <>
-      <nav >
+      <nav>
         <div className="flex w-[85%]  items-center mx-auto">
-          <img src="/src/assets/ForgedTrivia.png" alt="" className="w-[4%]  my-5" />
+          <img
+            src="/src/assets/ForgedTrivia.png"
+            alt=""
+            className="w-[4%]  my-5"
+          />
           <h1 className="text-2xl font-sans font-black ml-5">ForgedTrivia</h1>
           <ul className="flex w-full justify-end items-center gap-10 mr-10">
             <li>About</li>
