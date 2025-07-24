40f07cad496d5e8191114aaf36e45626eb74f9dd
hayyantahirr <hayyantahir7@gmail.com>
2025-07-25 00:53:16 +0500

    Css Added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,8 @@ const Navbar = () => {
   return (
     <>
       <nav >
-        <div className="flex w-full justify-start items-center container">
-          <img src="/src/assets/ForgedTrivia.png" alt="" className="w-[4%] mx-5 my-5" />
+        <div className="flex w-[85%]  items-center mx-auto">
+          <img src="/src/assets/ForgedTrivia.png" alt="" className="w-[4%]  my-5" />
           <h1 className="text-2xl font-sans font-black ml-5">ForgedTrivia</h1>
           <ul className="flex w-full justify-end items-center gap-10 mr-10">
             <li>About</li>
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,27 +1,146 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useRef, useState } from "react";
 
 const Home = () => {
+  const [questions, setQuestions] = useState(null);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [totalMarks, setTotalMarks] = useState(0);
+  const [result, setResult] = useState(0);
+  const selectedOptionRef = useRef();
+
+  useEffect(() => {
+    axios("https://the-trivia-api.com/v2/questions")
+      .then((res) => {
+        console.log(res.data);
+        setQuestions(res.data);
+        setTotalMarks(res.data.length * 10);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  function shuffleArray(arr) {
+    let newArr = arr.slice();
+
+    for (let i = newArr.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [newArr[i], newArr[j]] = [newArr[j], newArr[i]];
+    }
+
+    return newArr;
+  }
+
+  //   // next question
+  const nextQuestion = () => {
+    // get checked values
+    const selectedOption =
+      selectedOptionRef.current.querySelector("input:checked");
+    console.log(selectedOption.value);
+
+    if (questions[currentIndex].correctAnswer === selectedOption.value) {
+      console.log("sahi answer lagaya ha.");
+      setResult(result + 10);
+    } else {
+      console.log("galat answer haa");
+    }
+
+    if (currentIndex < questions.length - 1) {
+      setCurrentIndex(currentIndex + 1);
+    } else {
+      console.log("question khtm");
+    }
+  };
   return (
     <>
-      <div className=" mx-auto w-[85%] my-10">
-        <h1 className="text-3xl font-sans font-black mb-5 text-start ">Test Your Wit ,<span className="text-[#da7e36]"> Forge Your Legacy</span> </h1>
-        <p className="text-xl text-[#da7e36] opacity-70">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
-          voluptates facere accusantium dolore eligendi aliquid vero minima quam
-          odit at velit iste error totam aliquam optio dicta, perferendis
-          voluptatibus molestias? Dolorem quod cupiditate adipisci consequatur,
-          placeat ipsum. Inventore ipsam nam unde? Consequuntur ex quam fugit,
-          labore, sunt nihil eius quidem aliquam ipsa nesciunt voluptatum
-          possimus eveniet. Labore quidem libero magni! Laboriosam animi dolorum
-          voluptatem delectus quaerat, nihil temporibus quidem sapiente
-          distinctio eveniet? Eius deserunt alias sint dolorum beatae
-          accusantium iure quia, fuga earum nemo ut officia, atque consequatur,
-          molestiae non. Iure dolor ab, et exercitationem quasi debitis officia,
-          vel velit modi fugiat aliquid consectetur ut libero doloremque
-          provident officiis perferendis totam sunt? Eaque neque voluptatem est
-          doloremque eum odio ab?
-        </p>
-      </div>
+      <main>
+        <div className=" mx-auto w-[80%] my-10">
+          <h1 className="text-3xl font-sans font-black mb-5 text-start ">
+            Test Your Wit ,
+            <span className="text-[#da7e36]"> Forge Your Legacy</span>{" "}
+          </h1>
+          <p className="text-xl text-[#da7e36] opacity-70">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
+            voluptates facere accusantium dolore eligendi aliquid vero minima
+            quam odit at velit iste error totam aliquam optio dicta, perferendis
+            voluptatibus molestias? Dolorem quod cupiditate adipisci
+            consequatur, placeat ipsum. Inventore ipsam nam unde? Consequuntur
+            ex quam fugit, labore, sunt nihil eius quidem aliquam ipsa nesciunt
+            voluptatum possimus eveniet. Labore quidem libero magni! Laboriosam
+            animi dolorum voluptatem delectus quaerat, nihil temporibus quidem
+            sapiente distinctio eveniet?
+          </p>
+        </div>
+        <div className="flex flex-col w-[60%] mx-auto border  p-5 mb-5 rounded-xl  border-orange-500 shadow-[0_0_15px_2px_rgba(255,115,0,0.3)]  bg-[#0a0a0a] text-white">
+          <h1 className="text-end text-2xl mr-5 text-[#da7e36] font-sans font-black mb-5">
+            Result {result} / {totalMarks}
+          </h1>
+          {questions ? (
+            <div>
+              <h1 className="text-l flex items-center ml-1">
+                <span className="text-[#da7e36] font-sans font-black px-3 text-xl">
+                  Q{currentIndex + 1}:
+                </span>
+                {questions[currentIndex].question.text}
+              </h1>
+              <ul ref={selectedOptionRef} className="uiverse-pixel-radio-group">
+                {shuffleArray([
+                  ...questions[currentIndex].incorrectAnswers,
+                  questions[currentIndex].correctAnswer,
+                ]).map((item, index) => {
+                  return (
+                    <div>
+                      <li key={index} className="uiverse-pixel-radio">
+                        <input
+                          type="radio"
+                          value={item}
+                          name="pixel-choice"
+                          className="cursor-pointer"
+                        />
+                        <label htmlFor="" className="label-text ">
+                          {item}
+                        </label>
+                      </li>
+                      <li></li>
+                    </div>
+                  );
+                })}
+              </ul>
+
+              <h1><span className="text-[#da7e36] font-sans font-black px-3 text-xl">Difficulty :</span>  {questions[currentIndex].difficulty}</h1>
+
+              <button
+                onClick={nextQuestion}
+                className="relative inline-block p-px font-semibold leading-6 text-white bg-gray-800 shadow-2xl cursor-pointer rounded-xl shadow-zinc-900 transition-transform duration-300 ease-in-out hover:scale-105 active:scale-95 ml-3 mb-5"
+              >
+                <span className="absolute inset-0 rounded-xl bg-gradient-to-r from-teal-400 via-blue-500 to-purple-500 p-[2px] opacity-0 transition-opacity duration-500 group-hover:opacity-100"></span>
+
+                <span className="relative z-10 block px-6 py-3 rounded-xl bg-gray-950">
+                  <div className="relative z-10 flex items-center space-x-2">
+                    <span className="transition-all duration-500 group-hover:translate-x-1">
+                      Next Question
+                    </span>
+                    <svg
+                      className="w-6 h-6 transition-transform duration-500 group-hover:translate-x-1"
+                      data-slot="icon"
+                      aria-hidden="true"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        clip-rule="evenodd"
+                        d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z"
+                        fill-rule="evenodd"
+                      ></path>
+                    </svg>
+                  </div>
+                </span>
+              </button>
+            </div>
+          ) : (
+            <h1>Loading...</h1>
+          )}
+        </div>
+      </main>
     </>
   );
 };
